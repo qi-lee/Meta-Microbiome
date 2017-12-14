87c25d39343e7763721fa7790fdb8e711fdfc6cf
qi-lee <34326927+qi-lee@users.noreply.github.com>
2017-12-14 23:34:33 +0800

    Update found_essential_gene_by_group.pl

--- 5_Bins_Evaluation/found_essential_gene_by_group.pl
+++ 5_Bins_Evaluation/found_essential_gene_by_group.pl
@@ -1,15 +1,15 @@
 #!/usr/bin/perl
 use strict;
 
-############################################
+###############################################
 ##                ReadMe
-############################################
+###############################################
 ## Function: 
 ## Usage $: found_essential_gene_by_group.pl hmm_orf_alignment.txt group_1.list 
 ## Author: liqi (lab of algal genomics)
 ## Mail: liqi@ihb.ac.cn
 ## Created Time: Wed 09 Sep 2015 04:10:53 PM CST
-############################################
+###############################################
 
 
 open(Lib,$ARGV[0]);
